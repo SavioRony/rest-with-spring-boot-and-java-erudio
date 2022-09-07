1c3a518750b551a47ae87e209cd05812fc9dbce2
Savio Rony <savio_ls@hotmail.com>
2022-09-07 19:52:20 -0300

    Adicionado Content Negotiation para que os response e request possa ser enviado nos formatos de JSON, XML e YAML.

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -2,6 +2,7 @@ package br.com.erudio.controllers;
 
 import br.com.erudio.data.vo.v1.PersonVO;
 import br.com.erudio.services.PersonServices;
+import br.com.erudio.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,22 +15,27 @@ public class PersonController {
 
     @Autowired
     private PersonServices services;
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}",
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO findById(@PathVariable(value = "id") Long id) {
         return services.findById(id);
     }
 
-    @GetMapping()
-    public List<PersonVO> findAl() {
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
+    public List<PersonVO> findAll() {
         return services.findAll();
     }
 
-    @PostMapping()
+    @PostMapping(
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO save(@RequestBody PersonVO person) {
         return services.save(person);
     }
 
-    @PutMapping()
+    @PutMapping(
+            produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public PersonVO update(@RequestBody PersonVO person) {
         return services.update(person);
     }
